ee6a1f2cd08ccabb8b77695faf135aeab09b174b
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-06 00:33:21 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -27,7 +27,6 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  turn_count = 0
   if valid_move?(board, index)
     move(board, index)
     display_board(board)
